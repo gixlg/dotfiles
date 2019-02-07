388b23d4a9fe744aa59d529f3f5fa97c78b4faca
Luca Giuberti <gix_lg@yahoo.com>
2019-02-07 19:27:52 +0100

    Added fzf

---
+++ fzf/fzf.zsh
@@ -0,0 +1,14 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */Users/lgiubert/.fzf/bin* ]]; then
+  export PATH="$PATH:/Users/lgiubert/.fzf/bin"
+fi
+
+# Auto-completion
+# ---------------
+# [[ $- == *i* ]] && source "/Users/lgiubert/.fzf/shell/completion.zsh" 2> /dev/null
+
+# Key bindings
+# ------------
+# source "/Users/lgiubert/.fzf/shell/key-bindings.zsh"
+
