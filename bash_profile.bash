0eaef9f8ae1ddb55325f07bcdad3bf046d295014
Luca Giuberti <gix_lg@yahoo.com>
2020-03-05 00:34:13 +0100

    Removed some unused commands

--- bash_profile.bash
+++ bash_profile.bash
@@ -9,10 +9,7 @@ source $GITAWAREPROMPT/main.sh
 export PS1="\u@\h \w \[$txtcyn\]\$git_branch\[$txtred\]\$git_dirty\[$txtrst\]\$ "
 
 #npm completion
-source ~/.npm-completion
-
-#fzf https://github.com/junegunn/fzf
-#[ -f ~/.fzf.bash ] && source ~/.fzf.bash
+#source ~/.npm-completion
 
 #MySQL
 export PATH=$PATH:/usr/local/mysql/bin
@@ -21,11 +18,11 @@ alias openmysql="mysql -u root --password=password"
 #JAVA
 #do this to see all installed version:
 #/usr/libexec/java_home -V
-export JAVA_HOME=`/usr/libexec/java_home -v 1.8.0_144`
+#export JAVA_HOME=`/Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home`
 
 #Maven
-export M2_HOME=/usr/local/apache-maven-3.5.0
-export PATH=$PATH:$M2_HOME/bin
+#export M2_HOME=/usr/local/apache-maven-3.5.0
+#export PATH=$PATH:$M2_HOME/bin
 
 #IntellJ Diff
 alias diff-idea="/Applications/IntelliJ\ IDEA.app/Contents/MacOS/idea diff"
@@ -33,16 +30,6 @@ alias open-scratches='open ~/Library/Preferences/IntelliJIdea2018.2/scratches/'
 
 #Sublime
 alias sl="/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl"
-
-#-----------------------EPNM------------------------------
-#SSH
-#alias connect-dev-vm="ssh developer@vm-optdev-25.cisco.com -t \"cd git/storm; bash --login\""
-#alias connect-server-vm="ssh root@vm-optsrv-25.cisco.com -t \"cd /opt/CSCOlumos; bash --login\""
-
-#CDETS CLI
-export CDETS_INSTALL_DIR=~/Development/Cisco/cdets-unix
-export PATH=$PATH:$CDETS_INSTALL_DIR/bin
-#-----------------------EPNM------------------------------
 
 csvcolumn() { sed -e "s/^$2/ $2/" -e "s/$2$/$2 /" -e "s/$2$2/$2 $2/g"  -e "s/$2$2/$2 $2/g" $1 | column -t -s$2 ; }
 csvcomma() { sed -e 's/^,/ ,/' -e 's/,$/, /' -e 's/,,/, ,/g'  -e 's/,,/, ,/g' $1 | column -t -s, ; }
@@ -82,7 +69,6 @@ export LSCOLORS=ExFxCxDxBxegedabagacad
 
 #My Scripts
 export PATH=$PATH:~/bin
-source "$HOME/epnm/.epnm-rc"
 alias la='ls -al'
 alias ll='ls -l'
 
