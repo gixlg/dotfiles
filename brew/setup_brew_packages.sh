f328603577dd2fdd8b77c021e0ef8cf39668ee55
Luca Giuberti <gix_lg@yahoo.com>
2020-04-03 18:37:15 +0200

    Splitter brew script configuration from packages info

--- brew/setup_brew_packages.sh
+++ brew/setup_brew_packages.sh
@@ -1,68 +1,7 @@
 #!/bin/bash
 source ./utils/confirm.sh
 
-brew_packages=(
-    htop
-    wget
-    telnet
-    tree
-
-    maven
-    gradle
-    node
-
-    fzf #https://github.com/junegunn/fzf
-    jenv #https://github.com/jenv/jenv
-    bash-completion
-
-    exa # ls replacement https://github.com/ogham/exa
-    bat # cat replacement https://github.com/sharkdp/bat
-    diff-so-fancy # diff replacement https://github.com/so-fancy/diff-so-fancy
-    prettyping # ping replacement https://github.com/denilsonsa/prettyping
-    fd # user-friendly find alternative https://github.com/sharkdp/fd/
-    ncdu # better du
-    tldr # better man https://github.com/tldr-pages/tldr/
-
-    zsh-autosuggestions #https://github.com/zsh-users/zsh-autosuggestions
-    zsh-syntax-highlighting
-)
-
-brew_cask_packages=(
-    #google-chrome
-    slack
-    adoptopenjdk/openjdk/adoptopenjdk8
-    sublime-text
-    #zoomus
-    google-chat
-    skype
-    hyper
-    jumpcut
-    spotify
-    postman
-    docker
-    intellij-idea
-    visual-studio-code
-    google-drive-file-stream
-    dropbox
-    gimp
-    drawio
-    1password
-    dbeaver-community #https://stackoverflow.com/a/60912567/5945360
-
-    # Quick look plugins (https://github.com/sindresorhus/quick-look-plugins)
-    qlcolorcode
-    qlstephen
-    qlmarkdown
-    quicklook-json
-
-)
-
-brew_third_party_repositories=(
-    #homebrew/cask-cask
-    homebrew/cask-versions
-    adoptopenjdk/openjdk
-    
-)
+source ./brew/brew_packages.sh
 
 if [ "$1" == "--remove" ] || [ "$1" == "-r" ]; then
     confirm "Are you sure you want to disinstall Brew and all the packages?" || exit
