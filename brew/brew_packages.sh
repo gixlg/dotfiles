4be333c506dee2c7718e394d3c9d8643da10a7e0
Luca Giuberti <luca@192.168.1.57>
2021-08-14 18:44:42 +0200

    Added kubectx

--- brew/brew_packages.sh
+++ brew/brew_packages.sh
@@ -25,6 +25,7 @@ brew_packages=(
     tldr # better man https://github.com/tldr-pages/tldr/
     jq #https://stedolan.github.io/jq/
     awscli
+    kubectx #https://github.com/ahmetb/kubectx
     k9s #https://k9scli.io/
 
     zsh-autosuggestions #https://github.com/zsh-users/zsh-autosuggestions
